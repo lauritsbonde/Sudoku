b922a19877eca4e4182983ca43e779d2e884eae0
lauritsbonde <lauritsbonde@hotmail.dk>
2022-03-19 20:39:29 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Generator from './Components/Generator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Sudoku brother!🧠</h1>
+			<Generator />
+		</div>
+	);
 }
 
 export default App;
